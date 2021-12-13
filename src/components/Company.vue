e320dda2dadc9a28f39cf96571cf1ae3ee0966ce
adri567 <a.suthold@web.de>
2021-12-13 14:18:00 +0100

    Header

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -5,18 +5,21 @@
 </template>
 
 <script>
+
 export default {
   name: 'Company',
 
 }
 </script>
 
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .companybox-style {
   height: 100%;
   width: 100%;
-  background-color: black;
+  background-color: rgb(18,24,38); 
   border-radius: 25px;
+  box-shadow: 0 0 1em rgb(7, 16, 34);
 }
 </style>
